745b27ecf8af4c98e3264e709680ed663590840f
DemonRVO <47305650+DemonRVO@users.noreply.github.com>
2025-03-19 17:56:02 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export const PORT = process.env.PORT;
-export const MYSQLHOST = process.env.DB_HOST;
-export const MYSQLUSER = process.env.DB_USER;
-export const MYSQLPASSWORD = process.env.DB_PASSWORD;
-export const MYSQLDATABASE = process.env.DB_DATABASE;
-export const MYSQLPORT = process.env.DB_PORT;
+export const MYSQLHOST = process.env.MYSQLHOST;
+export const MYSQLUSER = process.env.MYSQLUSER;
+export const MYSQLPASSWORD = process.env.MYSQLPASSWORD;
+export const MYSQLDATABASE = process.env.MYSQLDATABASE;
+export const MYSQLPORT = process.env.MYSQLPORT;
